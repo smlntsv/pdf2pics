b4815d5717fd2332c7baa14256b82eb20e1c1196
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2024-11-30 17:09:29 +0600

    feat: Use Webpack CopyPlugin to copy pdfjs worker into public directory.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from 'next'
+import CopyPlugin from 'copy-webpack-plugin'
+import path from 'node:path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // reactStrictMode: true,
+  webpack: (config) => {
+    // To make it work with Next.js 15 install `ajv` package
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: require.resolve('pdfjs-dist/build/pdf.worker.mjs'),
+            to: path.resolve(__dirname, `public/static/js/pdf.worker.min.mjs`),
+          },
+        ],
+      })
+    )
+
+    return config
+  },
 }
 
 export default nextConfig
