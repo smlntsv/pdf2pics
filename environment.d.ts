da5af33b0d5233cbfc3b8fd1cee614192a1b5b6c
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2024-12-04 23:18:46 +0600

    feat: Add type declarations for env variables

---
+++ environment.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    readonly NEXT_PUBLIC_GITHUB_REPO_URL: string
+    readonly NEXT_PUBLIC_LINKEDIN_URL: string
+    readonly NEXT_PUBLIC_TWITTER_X_URL: string
+    readonly NEXT_PUBLIC_EMAIL: string
+  }
+}
