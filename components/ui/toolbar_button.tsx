953237c3e18ecb6e55c4f48d0c81e86d120cc5e5
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2024-12-08 00:17:04 +0600

    style: Support disable state for ToolbarButton. Select Arrow for select.

--- components/ui/toolbar_button.tsx
+++ components/ui/toolbar_button.tsx
@@ -1,18 +1,31 @@
 import { ButtonHTMLAttributes, FC } from 'react'
 import { cn } from '@/lib/utils'
 
-interface ToolbarButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-
-const ToolbarButton: FC<ToolbarButtonProps> = ({ className, children, ...reset }) => {
+const ToolbarButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  className,
+  children,
+  disabled,
+  ...reset
+}) => {
   return (
     <button
       className={cn(
-        'px-6 py-4 flex items-center font-medium',
-        'text-gray-500 hover:text-gray-600 active:text-gray-800',
-        'dark:text-white dark:hover:text-gray-100 dark:active:text-gray-200',
-        ' dark:hover:bg-blue-500 hover:bg-gray-50 active:bg-gray-100 rounded-lg',
+        'px-6 py-4 flex items-center rounded-lg',
+        'transition-colors duration-200 ease-in-out',
+        {
+          // Default state
+          'text-gray-800 hover:text-gray-600 active:text-gray-800 bg-transparent hover:bg-gray-50 active:bg-gray-100':
+            !disabled,
+          // Dark theme support
+          'dark:text-white dark:hover:text-gray-100 dark:active:text-gray-200 dark:hover:bg-blue-500':
+            !disabled,
+          // Disabled state
+          'text-gray-400 cursor-not-allowed': disabled,
+          'dark:text-gray-400 dark:cursor-not-allowed': disabled,
+        },
         className
       )}
+      disabled={disabled}
       {...reset}
     >
       {children}
