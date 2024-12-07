df6ff4d196035f91f102135097dc2250dc0b13f8
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2024-12-07 09:28:07 +0600

    feat: Update next.config.js to support SharedArrayBuffer. Remove CopyPlugin as unneeded.

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,16 @@
 import type { NextConfig } from 'next'
-import CopyPlugin from 'copy-webpack-plugin'
-import path from 'node:path'
 
 const nextConfig: NextConfig = {
-  // reactStrictMode: true,
-  webpack: (config) => {
-    // To make it work with Next.js 15 install `ajv` package
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: require.resolve('pdfjs-dist/build/pdf.worker.mjs'),
-            to: path.resolve(__dirname, `public/static/js/pdf.worker.min.mjs`),
-          },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
         ],
-      })
-    )
-
-    return config
+      },
+    ]
   },
 }
 
